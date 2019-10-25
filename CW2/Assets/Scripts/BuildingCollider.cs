10c3c083315b3dff8b4674d20b95cfc8ab039412
Aleksandr <ovcinikov@gmail.com>
2019-10-25 21:04:05 +0100

    Added simple Building system
    Added simple Building system, at the moment allows player to move Buildings even after setting them

---
+++ CW2/Assets/Scripts/BuildingCollider.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCollider : MonoBehaviour
+{
+    public Collider ObjectCollider => GetComponent<Collider>();
+}
---
+++ CW2/Assets/Scripts/BuildingScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingScript : MonoBehaviour
+{
+    private RaycastHit _hit;
+    private Ray _ray;
+    private Collider _collider;
+    private BuildingCollider ColliderBuilding => chosenBuilding.GetComponent<BuildingCollider>();
+    public GameObject chosenBuilding;
+    [SerializeField] private new Camera cam;
+    [SerializeField] private List<GameObject> buildings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                foreach (var building in buildings)
+                {
+                    if (building.transform == _hit.transform)
+                    {
+                        chosenBuilding = building;
+                        _collider = ColliderBuilding.ObjectCollider;
+                    }
+                }
+            }
+        }
+
+        if (chosenBuilding)
+        {
+            MoveObjectToMouse();
+            PlaceObject();
+        }
+    }
+    
+    private void MoveObjectToMouse()
+    {
+        _collider.enabled = false;
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            chosenBuilding.transform.position = _hit.point;
+            chosenBuilding.transform.rotation = Quaternion.FromToRotation(Vector3.up, _hit.normal);
+        }
+    }
+
+    private void PlaceObject()
+    {
+        if (chosenBuilding && Input.GetMouseButtonUp(0))
+        {
+            _collider.enabled = true;
+            chosenBuilding = null;
+        }
+    }
+}
