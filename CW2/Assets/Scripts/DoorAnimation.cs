00fa428cebb72a5dcc1f6d2785d0455f48675f28
Aleksandr <ovcinikov@gmail.com>
2020-02-17 19:34:25 +0000

    Modified index collider and door animation

--- CW2/Assets/Scripts/DoorAnimation.cs
+++ CW2/Assets/Scripts/DoorAnimation.cs
@@ -8,12 +8,15 @@ public class DoorAnimation : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Collider fingerCollider;
     private bool _doorState;
+    private bool _animationStarted;
+    private Renderer _buttonRenderer;
     private static readonly int DoorOpen = Animator.StringToHash("doorOpen");
 
     // Start is called before the first frame update
     void Start()
     {
         _doorState = false;
+        _buttonRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -24,8 +27,20 @@ public class DoorAnimation : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(_animationStarted) return;
         if(other != fingerCollider) return;
+        StartCoroutine(UseAnimation());
+
+    }
+
+    private IEnumerator UseAnimation()
+    {
+        _animationStarted = true;
+        _buttonRenderer.material.EnableKeyword("_EMISSION");
         _doorState = !_doorState;
         animator.SetBool(DoorOpen, _doorState);
+        yield return new WaitForSeconds(3);
+        _animationStarted = false;
+        _buttonRenderer.material.DisableKeyword("_EMISSION");
     }
 }
