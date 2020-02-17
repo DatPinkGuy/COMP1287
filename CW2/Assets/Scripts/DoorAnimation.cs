70da5af3dfbe06e5e472ec3a9adabb4c6395d10d
Aleksandr <ovcinikov@gmail.com>
2020-02-17 13:24:43 +0000

    Working on animating doors

---
+++ CW2/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private bool _doorState;
+    private static readonly int DoorOpen = Animator.StringToHash("doorOpen");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetBool(DoorOpen, _doorState);
+        _doorState = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _doorState = !_doorState;
+    }
+}
